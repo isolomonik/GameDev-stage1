22e1de8cb4cf1decd0281987c4cae5b28b76a746
Irina <irina.solomoni2@gmail.com>
2016-11-03 16:10:20 +0200

    Added light

---
+++ stage1/Assets/DoorTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour {
+    public GameObject door;
+
+    void OnTriggerEnter(Collider smbd) {
+        if (smbd.CompareTag("Player")) { 
+       // door.GetComponent<MeshRenderer>().
+       }
+	}
+
+    void OnTriggerExit(Collider smbd)
+    {
+        if (smbd.CompareTag("Player"))
+        {
+            // door.GetComponent<MeshRenderer>().
+        }
+    }
+}
