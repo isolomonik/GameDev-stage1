ff9144f10709059f87e75222a61b4a62dc2b8943
isolomonik <irina.solomonik2@gmail.com>
2016-11-16 01:16:32 +0500

    dark room

---
+++ stage1/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DoorTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider smbd)
+    {
+        if (smbd.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
