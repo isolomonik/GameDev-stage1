b448436a5922ff62b13ce582e5fc78e5b05524a9
isolomonik <irina.solomonik2@gmail.com>
2016-11-17 01:02:37 +0500

    some detales

---
+++ stage1/Assets/Scripts/L2LightOn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class L2LightOn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp("e"))
+
+        {
+            }
+        }
+}
