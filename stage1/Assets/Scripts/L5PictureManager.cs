84757003f3e44c08b58c3bacf7848879f220f9b7
Irina <irina.solomoni2@gmail.com>
2016-12-06 17:00:02 +0200

    fixed

--- stage1/Assets/Scripts/L5PictureManager.cs
+++ stage1/Assets/Scripts/L5PictureManager.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class L5PictureManager : MonoBehaviour {
 
+    public GameObject ButtonMore; 
+
+    private GameObject[] _pictures;
+    private float _maxDistance = 3;
+    private GameObject _actualPicture;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        _pictures = GameObject.FindGameObjectsWithTag("Picture");
+        _actualPicture = _pictures[0];
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        float dist = (transform.position - _actualPicture.transform.position).sqrMagnitude;
+        foreach (GameObject picture in _pictures) {
+            if ((transform.position - picture.transform.position).sqrMagnitude <dist )
+            {
+                _actualPicture = picture;
+                dist = (transform.position - picture.transform.position).sqrMagnitude;
+            }
+        }
+        
+        if ( (transform.position - _actualPicture.transform.position).sqrMagnitude < _maxDistance * _maxDistance)
+        {
+            ButtonMore.SetActive(true);
+        }
+        else
+        {ButtonMore.SetActive(false);
+
+        }
+
+        }
 }
