c9f53573cba57f4238bd881abdf29563b6a11ee1
isolomonik <irina.solomonik2@gmail.com>
2016-12-12 18:38:38 +0500

    fixed, closed issue

---
+++ stage1/Assets/Scripts/L5Picture.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+namespace Assets.Scripts
+{
+    public class L5Picture : MonoBehaviour
+    {
+        public string PictureName;
+        public string FileNameInfo;
+
+
+        public string GetName()
+        {
+            return PictureName;
+        }
+
+        public string ShowPictureInfo()
+        {
+            return ((TextAsset) Resources.Load(FileNameInfo)).text;
+        }
+    }
+}
--- stage1/Assets/Scripts/L5PictureManager.cs
+++ stage1/Assets/Scripts/L5PictureManager.cs
@@ -1,40 +1,47 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
-public class L5PictureManager : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class L5PictureManager : MonoBehaviour
+    {
+        public GameObject ButtonMore;
+        public Text PictureInfo;
+        public Text MorePictureInfoText;
 
-    public GameObject ButtonMore; 
+        private GameObject[] _pictures;
+        private float _maxDistance = 3;
+        private GameObject _actualPicture;
 
-    private GameObject[] _pictures;
-    private float _maxDistance = 3;
-    private GameObject _actualPicture;
-
-	// Use this for initialization
-	void Start () {
-        _pictures = GameObject.FindGameObjectsWithTag("Picture");
-        _actualPicture = _pictures[0];
-
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        float dist = (transform.position - _actualPicture.transform.position).sqrMagnitude;
-        foreach (GameObject picture in _pictures) {
-            if ((transform.position - picture.transform.position).sqrMagnitude <dist )
-            {
-                _actualPicture = picture;
-                dist = (transform.position - picture.transform.position).sqrMagnitude;
-            }
-        }
-        
-        if ( (transform.position - _actualPicture.transform.position).sqrMagnitude < _maxDistance * _maxDistance)
+        // Use this for initialization
+        void Start()
         {
-            ButtonMore.SetActive(true);
-        }
-        else
-        {ButtonMore.SetActive(false);
-
+            _pictures = GameObject.FindGameObjectsWithTag("Picture");
+            _actualPicture = _pictures[0];
         }
 
+        // Update is called once per frame
+        void Update()
+        {
+            float dist = (transform.position - _actualPicture.transform.position).sqrMagnitude;
+            foreach (GameObject picture in _pictures)
+            {
+                if ((transform.position - picture.transform.position).sqrMagnitude < dist)
+                {
+                    _actualPicture = picture;
+                    dist = (transform.position - picture.transform.position).sqrMagnitude;
+                }
+            }
+
+            ButtonMore.SetActive((transform.position - _actualPicture.transform.position).sqrMagnitude <
+                                 _maxDistance*_maxDistance);
+            PictureInfo.text = _actualPicture.GetComponent<Assets.Scripts.L5Picture>().GetName();
+            MorePictureInfoText.text = _actualPicture.GetComponent<Assets.Scripts.L5Picture>().ShowPictureInfo();
         }
+
+        public void ButtonOnClick()
+        {
+            Debug.Log("Pressed");
+        }
+    }
 }
